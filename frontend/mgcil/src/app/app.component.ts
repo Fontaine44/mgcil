97f49ea9775035b91565b7cb787d0d1320180259
Fontaine44 <raphael.fontaine@mail.mcgill.ca>
2025-03-11 11:04:52 -0400

    Add closure

--- frontend/mgcil/src/app/app.component.ts
+++ frontend/mgcil/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./shared/components/navbar/navbar.component";
 import { AlertComponent } from './shared/components/alert/alert.component';
+import { ClosureComponent } from "./shared/components/closure/closure.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, AlertComponent],
+  imports: [RouterOutlet, NavbarComponent, AlertComponent, ClosureComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ frontend/mgcil/src/app/shared/components/closure/closure.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-closure',
+  standalone: true,
+  imports: [],
+  templateUrl: './closure.component.html',
+  styleUrl: './closure.component.scss'
+})
+export class ClosureComponent {
+
+}
